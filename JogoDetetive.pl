5d533e52e8675f37bd3d142b3581a358684098c8
Thaiane Braga <thaiane.unb@hotmail.com>
2013-10-23 18:33:02 -0200

    Corrigindo sintaxe

--- JogoDetetive.pl
+++ JogoDetetive.pl
@@ -1,6 +1,6 @@
-suspeito( "coronel mostarda" ).
-suspeito( "dona branca" ).
-suspeito( "senhor marinho" ).
-suspeito( "dona violeta" ).
-suspeito( "professor black" ).
-suspeito( "senhorita rosa" ).
+suspeito('coronel mostarda').
+suspeito('dona branca').
+suspeito('senhor marinho').
+suspeito('dona violeta').
+suspeito('professor black').
+suspeito('senhorita rosa').
