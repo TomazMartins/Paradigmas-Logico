86ab0e4fc13d52c48ab40554433ea254f93ecefa
Thaiane Braga <thaiane.unb@hotmail.com>
2013-10-23 18:57:16 -0200

    Inclusão das armas.

--- JogoDetetive.pl
+++ JogoDetetive.pl
@@ -4,3 +4,13 @@ suspeito('senhor marinho').
 suspeito('dona violeta').
 suspeito('professor black').
 suspeito('senhorita rosa').
+
+arma(corda).
+arma(punhal).
+arma(cano).
+arma('chave inglesa').
+arma('veneno').
+arma(castical).
+arma(revolver).
+arma(espingarda).
+arma('soco ingles').
