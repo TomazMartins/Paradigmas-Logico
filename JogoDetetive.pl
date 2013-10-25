93e2b4e2156d19acf868ed358fc571a31dbf8f3f
Thaiane Braga <thaiane.unb@hotmail.com>
2013-10-25 03:34:49 -0200

    Acrescentando caso 2

--- JogoDetetive.pl
+++ JogoDetetive.pl
@@ -32,37 +32,63 @@ local(hall).
 local('sala de estar').
 local(adega).
 
-sabeQueNao('coronel mostarda',suspeito,'dona branca').
-sabeQueNao('coronel mostarda',arma,corda).
-sabeQueNao('coronel mostarda',arma,punhal).
-sabeQueNao('coronel mostarda',local,biblioteca).
+sabeQueNao(caso1,'coronel mostarda',suspeito,'dona branca').
+sabeQueNao(caso1,'coronel mostarda',arma,corda).
+sabeQueNao(caso1,'coronel mostarda',arma,punhal).
+sabeQueNao(caso1,'coronel mostarda',local,biblioteca).
+sabeQueNao(caso2,'coronel mostarda',suspeito,'dona branca').
+sabeQueNao(caso2,'coronel mostarda',arma,corda).
+sabeQueNao(caso2,'coronel mostarda',arma,punhal).
+sabeQueNao(caso2,'coronel mostarda',local,biblioteca).
 
-sabeQueNao('dona branca',susteito,'dona violeta').
-sabeQueNao('dona branca',arma,veneno).
-sabeQueNao('dona branca',local,'salao de jogos').
-sabeQueNao('dona branca',local, adega).
+sabeQueNao(caso1,'dona branca',susteito,'dona violeta'). 
+sabeQueNao(caso1,'dona branca',arma,veneno).
+sabeQueNao(caso1,'dona branca',local,'salao de jogos').
+sabeQueNao(caso1,'dona branca',local, adega).
+sabeQueNao(caso2,'dona branca',susteito,'professor black'). 
+sabeQueNao(caso2,'dona branca',arma,espingarda).
+sabeQueNao(caso2,'dona branca',local,'salao de jogos').
+sabeQueNao(caso2,'dona branca',local, adega).
 
-sabeQueNao('senhor marinho',suspeito,desconhecido).
-sabeQueNao('senhor marinho',arma,'chave inglesa').
-sabeQueNao('senhor marinho',local,escritorio).
-sabeQueNao('senhor marinho',local,entrada).
-sabeQueNao('senhor marinho',local,'sala de musica').
+sabeQueNao(caso1,'senhor marinho',suspeito,desconhecido).
+sabeQueNao(caso1,'senhor marinho',arma,'chave inglesa').
+sabeQueNao(caso1,'senhor marinho',local,escritorio).
+sabeQueNao(caso1,'senhor marinho',local,entrada).
+sabeQueNao(caso1,'senhor marinho',local,'sala de musica').
+sabeQueNao(caso2,'senhor marinho',suspeito,desconhecido).
+sabeQueNao(caso2,'senhor marinho',arma,'chave inglesa').
+sabeQueNao(caso2,'senhor marinho',local,escritorio).
+sabeQueNao(caso2,'senhor marinho',local,entrada).
+sabeQueNao(caso2,'senhor marinho',local,'sala de musica').
 
-sabeQueNao('dona violeta',suspeito,'senhor marinho').
-sabeQueNao('dona violeta',suspeito,'senhorita rosa').
-sabeQueNao('dona violeta',arma,castical).
-sabeQueNao('dona violeta',local,'sala de jantar').
+sabeQueNao(caso1,'dona violeta',suspeito,'senhor marinho').
+sabeQueNao(caso1,'dona violeta',suspeito,'senhorita rosa').
+sabeQueNao(caso1,'dona violeta',arma,castical).
+sabeQueNao(caso1,'dona violeta',local,'sala de jantar').
+sabeQueNao(caso2,'dona violeta',suspeito,'senhor marinho').
+sabeQueNao(caso2,'dona violeta',suspeito,'senhorita rosa').
+sabeQueNao(caso2,'dona violeta',arma,castical).
+sabeQueNao(caso2,'dona violeta',local,'sala de jantar').
 
-sabeQueNao('professor black',suspeito,desconhecido).
-sabeQueNao('professor black',arma,revolver).
-sabeQueNao('professor black',arma,espingarda).
-sabeQueNao('professor black',local,cozinha).
-sabeQueNao('professor black',local,'salao de festas').
+sabeQueNao(caso1,'professor black',suspeito,desconhecido).
+sabeQueNao(caso1,'professor black',arma,revolver).
+sabeQueNao(caso1,'professor black',arma,espingarda).
+sabeQueNao(caso1,'professor black',local,cozinha).
+sabeQueNao(caso1,'professor black',local,'salao de festas').
+sabeQueNao(caso2,'professor black',suspeito,desconhecido).
+sabeQueNao(caso2,'professor black',arma,revolver).
+sabeQueNao(caso2,'professor black',arma,cano).
+sabeQueNao(caso2,'professor black',local,hall).
+sabeQueNao(caso2,'professor black',local,'salao de festas').
 
-sabeQueNao('senhorita rosa',suspeito,'coronel mostarda').
-sabeQueNao('senhorita rosa',arma,'soco ingles').
-sabeQueNao('senhorita rosa',local,hall).
-sabeQueNao('senhorita rosa',local,'sala de estar').
+sabeQueNao(caso1,'senhorita rosa',suspeito,'coronel mostarda').
+sabeQueNao(caso1,'senhorita rosa',arma,'soco ingles').
+sabeQueNao(caso1,'senhorita rosa',local,hall).
+sabeQueNao(caso1,'senhorita rosa',local,'sala de estar').
+sabeQueNao(caso2,'senhorita rosa',suspeito,'coronel mostarda').
+sabeQueNao(caso2,'senhorita rosa',arma,'soco ingles').
+sabeQueNao(caso2,'senhorita rosa',local,jardim).
+sabeQueNao(caso2,'senhorita rosa',local,'sala de estar').
 
 possivelCrime(Susteito,Arma,Local) :- suspeito(Suspeito),arma(Arma),local(Local).
 
@@ -74,27 +100,27 @@ start :- tab(15),write('Seja bem-vindo a Delegacia Prolog!'),nl,
          tab(15),write('Aqui solucionamos os crimes utilizando raciocionio logico.'),nl,nl,
          tab(15),write('Qual o seu nome, agente?'),nl,read(X),nl,
          tab(15),write('Ola, agente '),write(X),write(', execute caso1 para verificar os dados do seu primeiro caso.'),nl,
-	 tab(15),write('Bom trabalho!'),nl.
+         tab(15),write('Bom trabalho!'),nl.
 
 caso1 :- tab(15),write('Um grande empresario foi assassinado dentro da sua propria casa.'),nl,
-	 tab(15),write('Alguns suspeitos ja foram identificados e levados para a casa do falecido.'),nl,
-	 tab(15),write('Os possiveis locais e armas ja estao inclusos no seu banco de dados.'),nl,
-	 tab(15),write('Interroge os suspeitos e atualize o banco de forma a auxiliar na conclusao do caso.'),nl,
-	 tab(15),write('Quando tiver solucionado, execute solucao para informar o seu superior sobre o suspeito, local e arma do crime.'),nl,nl,
-	 tab(15),write('Bom trabalho!'),nl.
+         tab(15),write('Alguns suspeitos ja foram identificados e levados para a casa do falecido.'),nl,
+         tab(15),write('Os possiveis locais e armas ja estao inclusos no seu banco de dados.'),nl,
+         tab(15),write('Interroge os suspeitos e atualize o banco de forma a auxiliar na conclusao do caso.'),nl,
+         tab(15),write('Quando tiver solucionado, execute solucao para informar o seu superior sobre o suspeito, local e arma do crime.'),nl,nl,
+         tab(15),write('Bom trabalho!'),nl.
 
-pergunta(Testemunha,Sobre,Suspeita) :- 	sabeQueNao(Testemunha,Sobre,Suspeita),write(Testemunha),write(' sabe que '), 
-					write(Suspeita),write(' nao tem relacao com o crime.'),nl,!;write(Testemunha),
-					write(' nao sabe se '), write(Suspeita),write(' tem relacao com o crime.'),nl.
+pergunta(Caso,Testemunha,Sobre,Suspeita) :- sabeQueNao(Caso,Testemunha,Sobre,Suspeita),write(Testemunha),write(' sabe que '), 
+                                        write(Suspeita),write(' nao tem relacao com o crime.'),nl,!;write(Testemunha),
+                                        write(' nao sabe se '), write(Suspeita),write(' tem relacao com o crime.'),nl,nl,nl.
 
 solucao :- tab(15),write('Solucionou o caso, agente? Parabens! Diga-me quem eh o criminoso?'),nl,
-	   tab(15),read(Criminoso),nl,tab(15),write('E qual foi o local?'),nl,tab(15),read(Local),
-	   nl,tab(15), write('E qual foi a arma usada?'),nl,tab(15), read(Arma),
-	   Criminoso = 'professor black', Local = jardim, Arma = cano, nl,nl,tab(15),write('Aguarde um instante, agente...'),nl,nl,
-	   tab(15), write('Parabens! voce acertou! Professor Black acaba de confessar e o crime ocorreu como voce disse.'),nl,
-	   tab(15), write('Continue assim que logo sera promovido.'),nl,tab(15),write('Ate a proxima!'),nl;nl,nl,tab(15),
-	   write('Aguarde um instante, agente...'),nl,nl,
-	   tab(15), write('Lamento, voce errou. Existem testemunhas que sabem que nao foi desse jeito. Investigue mais um pouco...'),nl,nl.
+           tab(15),read(Criminoso),nl,tab(15),write('E qual foi o local?'),nl,tab(15),read(Local),
+           nl,tab(15), write('E qual foi a arma usada?'),nl,tab(15), read(Arma),
+           Criminoso = 'professor black', Local = jardim, Arma = cano, nl,nl,tab(15),write('Aguarde um instante, agente...'),nl,nl,
+           tab(15), write('Parabens! voce acertou! Professor Black acaba de confessar e o crime ocorreu como voce disse.'),nl,
+           tab(15), write('Continue assim que logo sera promovido.'),nl,tab(15),write('Ate a proxima!'),nl;nl,nl,tab(15),
+           write('Aguarde um instante, agente...'),nl,nl,
+           tab(15), write('Lamento, voce errou. Existem testemunhas que sabem que nao foi desse jeito. Investigue mais um pouco...'),nl,nl.
 
 banco :- listarSuspeitos,listarArmas,listarLocais.
 
@@ -102,4 +128,4 @@ listarSuspeitos :-listing(suspeito).
 listarArmas:- listing(arma).
 listarLocais :- listing(local).
 
-randomico(X) :- Y is rand(100) + 1, X is Y - fp(Y).
+escolhendoCaso(X) :- Y is rand(2) + 1, X is Y - fp(Y), write('Nao esqueca o numero do seu caso, agente.'),nl,nl.
